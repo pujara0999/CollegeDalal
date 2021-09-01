1a24c09e0f890ff59969ccff2eb943065603c6e4
Pramit Pujara <pujara9910pramit@gmail.com>
2021-09-01 17:03:40 +0530

    removed mongoose error

--- index.js
+++ index.js
@@ -18,13 +18,9 @@ app.use(bodyParser.urlencoded({
 }))
 
 mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify: true})
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database Connected...');
-}).catch(err => {
-    console.log('Connection Failed');
-});
-
+mongoose.connection.on('error', err => {
+    console.log(err);
+  });
 
 var db = mongoose.connection;
 
